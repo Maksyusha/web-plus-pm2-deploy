8915142b270fdf5e798d20d9be6b2e876f45cd65
Maksyusha <pirate227@yandex.ru>
2023-09-13 12:31:03 +0300

    added ecosustem files

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,27 @@
+require('dotenv').config({path: '.env.deploy'});
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REPO, DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps: [
+    {
+      name: "service",
+      script: "./dist/app.js",
+    },
+  ],
+
+  // Настройка деплоя
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
+      'post-deploy': 'cd /home/pirate227/mesto-project/source/backend/ && npm i && npm run build',
+    },
+  },
+};
---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,21 @@
+require('dotenv').config({path: '.env.deploy'});
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REPO, DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps: [],
+
+  // Настройка деплоя
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'post-deploy': 'cd /home/pirate227/mesto-project/source/frontend/ && npm i && npm run build',
+    },
+  },
+};
